1ddbe63db3ee94374addfd25199ab0984e11ef01
cjc <bluecats2010@163.com>
2023-08-07 22:25:43 +0800

    one

--- crm-springmvc/src/main/java/com/powernode/crm/workbench/service/inter/DicValueService.java
+++
@@ -1,9 +0,0 @@
-package com.powernode.crm.workbench.service.inter;
-
-import com.powernode.crm.settings.model.DicValue;
-
-import java.util.List;
-
-public interface DicValueService {
-    List<DicValue> queryDicValueByDicTypeCode(String typeCode);
-}
